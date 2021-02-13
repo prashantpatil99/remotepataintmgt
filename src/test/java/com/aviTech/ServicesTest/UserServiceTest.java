a76e9fa2250c7c3de319f0b8a10c74e841f71527
Prashant Patil <patil.prashant100399@g,ail.com>
2021-02-13 16:05:02 +0530

    comment test case for  saveUserTest()

--- src/test/java/com/aviTech/ServicesTest/UserServiceTest.java
+++ src/test/java/com/aviTech/ServicesTest/UserServiceTest.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@SpringBootTest
 public class UserServiceTest {
 
     @Autowired
@@ -19,7 +19,7 @@ public class UserServiceTest {
     @MockBean
     private UsersRepository usersRepository ;
 
-
+    @Test
     public void saveUserTest(){
         Users users=new Users( 2,  "Hanmant Tone",  "hanmanttone@gmail.com",  "6789543", "yelavi", "Hanmant Tone", "13-FEB-2021",  "Hanmant Tone", "13-FEB-2021", 2);
         when(usersRepository.save(users)).thenReturn(users);
